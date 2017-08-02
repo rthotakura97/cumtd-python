5878d4001fa784e54d6be5324a56c041d34ca2c9
Rohit Thotakaura <rthotakura97@gmail.com>
2017-08-01 20:05:08 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,12 @@
-from distutils.core import setup
-
+from setuptools import setup, find_packages
 
 setup(
     name='cumtd-python',
     version='1.0.0',
-    packages=['cumtd'],
-    license='MIT'
+    packages=find_packages(),
+    license='MIT',
     url='https://github.com/rthotakura97/cumtd-python',
     author='Rohit Thotakura',
     author_email='rthotakura97@gmail.com',
     description='An unnoficial Python client for the CUMTD REST API. '
-    classifiers=[
-     'Development Status :: 5 - Production/Stable',
-     'Intended Audience :: Developers',
-     'Topic :: Software Development :: Libraries :: Python Modules',
-     'License :: OSI Approved :: MIT License',
-     'Programming Language :: Python :: 2',
-     'Programming Language :: Python :: 3',   
-    ]
-    keywords=['cumtd','api','champaign','urbana','transit']
-)
+   )
