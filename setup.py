9591a0cd386b4c1a82fcf956da7f104d64a9ba47
Rohit Thotakaura <rthotakura97@gmail.com>
2017-08-22 16:49:30 -0400

    updated setup.py w/ keywords

--- setup.py
+++ setup.py
@@ -9,5 +9,6 @@ setup(
     url='https://github.com/rthotakura97/cumtd-python',
     author='Rohit Thotakura',
     author_email='rthotakura97@gmail.com',
-    description='An unnoficial Python client for the CUMTD REST API. '
+    description='An unnoficial Python client for the CUMTD REST API.',
+    keywords=['cumtd', 'bus', 'champaign', 'urbana', 'transit']
    )
